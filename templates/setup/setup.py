1e5877b20e7d961779459c3845517cee8a24aac3
Muhamad Azmy <azmy@conictus.com>
2018-03-04 19:19:51 +0200

    Setup installing

--- templates/setup/setup.py
+++ templates/setup/setup.py
@@ -12,6 +12,7 @@ class Setup(TemplateBase):
 
     VDC_TEMPLATE = 'github.com/openvcloud/0-templates/vdc/0.0.1'
     NODE_TEMPLATE = 'github.com/openvcloud/0-templates/node/0.0.1'
+    ZROBOT_TEMPLATE = 'github.com/openvcloud/0-templates/zrobot/0.0.1'
 
     def __init__(self, name, guid=None, data=None):
         super().__init__(name=name, guid=guid, data=data)
@@ -27,7 +28,7 @@ class Setup(TemplateBase):
         name = '%s-little-helper'
         nodes = self.api.services.find(template_uid=self.NODE_TEMPLATE, name=name)
         if len(nodes) != 0:
-            return j.tools.nodemgr.get(name).prefab
+            return nodes[0]
 
         # create the node instead
         # create a disk service
@@ -44,7 +45,39 @@ class Setup(TemplateBase):
         # update data in the disk service
         task = node.schedule_action('install')
         task.wait()
-        # check status ?
+        if task.state == 'error':
+            raise task.eco
+
+        return node
+
+    def _ensure_zrobot(self, helper):
+        name = '%s-little-bot' % self.name
+        bots = self.api.services.find(template_uid=self.ZROBOT_TEMPLATE, name=name)
+        if len(bots) != 0:
+            return bots[0]
+
+        # create the node instead
+        # create a disk service
+        bot = self.api.services.create(
+            template_uid=self.ZROBOT_TEMPLATE,
+            service_name=name,
+            data={
+                'node': helper.name,
+                'port': 6600,
+                'templates': [
+                    'https://github.com/openvcloud/0-templates.git',
+                    'https://github.com/openvcloud/kubernetes.git',
+                ],
+            },
+        )
+
+        # update data in the disk service
+        task = bot.schedule_action('install')
+        task.wait()
+        if task.state == 'error':
+            raise task.eco
+
+        return bot
 
     def install(self):
         try:
@@ -53,8 +86,7 @@ class Setup(TemplateBase):
         except StateCheckError:
             pass
 
-        # create the helper machine (size 1 may be)
-        # install zrobot on that node
-        # > push a bp
+        helper = self._ensure_helper()
+        self._ensure_zrobot(helper)
 
         self.state.set('actions', 'install', 'ok')
